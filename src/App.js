0484879ce409c9325b854c912a22bfdb1e10456a
Mlk777 <>
2020-04-13 12:56:01 +0200

    First draft

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import ImageCard from './components/ImageCard';
+import ImageSearch from './components/ImageSearch';
 
-function App() {
+const App = () => {
+  const [images, setImages] = useState([]);
+  const [isLoading, setIsLoading] = useState('true');
+  const [searchTerm, setSearchTerm] = useState('');
+
+  useEffect(() => {
+    try {
+      (async () => {
+        const request = await fetch(
+          `https://pixabay.com/api/?key=${process.env.REACT_APP_PIXABAY_API_KEY}&q=${searchTerm}&image_type=photo&pretty=true`
+        );
+        const data = await request.json();
+        setImages(data.hits);
+        setIsLoading(false);
+      })();
+    } catch (err) {
+      console.log(err);
+    }
+    //eslint-disable-next-line
+  }, [searchTerm]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <ImageSearch searchText={text => setSearchTerm(text)} />
+      {!isLoading && images.length === 0 && (
+        <h1 className='text-5xl text-center mx-auto mt-32'>
+          No images found for this term
+        </h1>
+      )}
+      {isLoading ? (
+        <h1 className='text-6xl text-center mx-auto mt-32'>Loading...</h1>
+      ) : (
+        <div className='w-full flex md:flex-row justify-around m-4'>
+          <div className='grid grid-cols-1 md:grid-cols-2 gap-20 '>
+            {images &&
+              images.map(image => {
+                return <ImageCard key={image.id} image={image} />;
+              })}
+          </div>
+        </div>
+      )}
+    </>
   );
-}
+};
 
 export default App;
---
+++ src/components/ImageCard.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import '../assets/modal.css';
+import Modal from './Modal';
+import useModal from '../hooks/useModal';
+
+const ImageCard = ({ image }) => {
+  const tags = image.tags.split(',');
+  const { isOpen, toggle } = useModal();
+
+  return (
+    <div className='max-w-sm rounded overflow-hidden shadow-lg'>
+      <img
+        src={image.webformatURL}
+        alt='random pic from api'
+        className='w-full'
+        style={{ height: '400px', width: '640px' }}
+        onClick={toggle}
+      />
+      <Modal isOpen={isOpen} hide={toggle} image={image.largeImageURL} />
+      <div className='px-6 py-4'>
+        <div className='flex items-center mb-2'>
+          <div className='font-bold text-purple-500 text-xl mb-2 mr-auto'>
+            Photo by {image.user}
+          </div>
+          {image.userImageURL ? (
+            <img
+              src={image.userImageURL}
+              alt='user profile pic'
+              className='rounded-full h-12 w-12'
+            />
+          ) : (
+            <img
+              src=''
+              alt=''
+              className='h-12 w-12 rounded-full border-2 border-gray-300'
+            />
+          )}
+        </div>
+        <ul>
+          <li>
+            <strong>Views: </strong>
+            {image.views}
+          </li>
+          <li>
+            <strong>Downloads: </strong>
+            {image.downloads}
+          </li>
+          <li>
+            <strong>Likes: </strong>
+            {image.likes}
+          </li>
+        </ul>
+      </div>
+      <div className='px-4 py-4 mt-2 w-12/12 border-t-2 border-teal-100 flex justify-start'>
+        {tags.map((tag, index) => (
+          <span
+            key={index}
+            className='bg-gray-200 rounded-full px-3 py-1 text-sm font-bold text-gray-700 mr-2 m-1'
+          >
+            #{tag}
+          </span>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default ImageCard;
