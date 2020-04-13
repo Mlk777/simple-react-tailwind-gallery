edc2ef79c97e8dee0a09527202d9699404619880
Mlk777 <>
2020-04-13 13:11:26 +0200

    Updates

---
+++ precache-manifest.a52be0be4d170070bdf1660eb0af8f56.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8aa79c9d1b70c97a7db7eb131d851cc",
+    "url": "/simple-react-tailwind-gallery/index.html"
+  },
+  {
+    "revision": "363523c9626aa1650117",
+    "url": "/simple-react-tailwind-gallery/static/css/main.99fd0d0c.chunk.css"
+  },
+  {
+    "revision": "0088e8f1d29f9b2b3503",
+    "url": "/simple-react-tailwind-gallery/static/js/2.d62d1ba2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/simple-react-tailwind-gallery/static/js/2.d62d1ba2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "363523c9626aa1650117",
+    "url": "/simple-react-tailwind-gallery/static/js/main.5da54319.chunk.js"
+  },
+  {
+    "revision": "8b1cf6848c7a59df285e",
+    "url": "/simple-react-tailwind-gallery/static/js/runtime-main.c309dc64.js"
+  }
+]);
